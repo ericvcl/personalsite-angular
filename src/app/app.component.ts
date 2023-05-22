a439a0e49c6bc600f32ee8dd9f60213a5641d223
ericvcl <eric.latham.2@gmail.com>
2023-05-21 22:23:34 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, HostListener, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal, { centered: true, size: 'lg' })
+  }
+
+  changeThemeColor(){
+    const body = document.getElementsByClassName('bg')[0]
+    body.classList.toggle("dark")
+    body.classList.toggle("light")
+  }
+
+  scrolled: boolean = false;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 50;
+  }
+}
